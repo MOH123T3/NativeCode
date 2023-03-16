95846d20e4bdbab86b99e5216a3d5859e05e1fb5
Mohit Panchal <96102522+MOH123T3@users.noreply.github.com>
2023-03-16 12:06:56 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,53 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Native Code {Android}'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  static const channel = MethodChannel('com.example.nativecode');
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+          child: ElevatedButton(
+              onPressed: () {
+                native();
+              },
+              child: Text("Show"))),
+    );
+  }
+
+  native() async {
+    await channel.invokeMethod('showBody',
+        <String, String>{'msg': 'This is from Android Native CODE'});
+  }
+}
